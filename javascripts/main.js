f4b696e21bb0fdc58f998aa6552f73a5aef663c3
J-Et. MARTIN <jetmartin@1998622.no-reply.drupal.org>
2014-02-17 08:53:22 +0100

    Pages update.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+	$("#toc").append('<h4><a href="#" name="toc"></a>Table of contents</h4><ul></ul>')
+	$("#main_content h2").each(function(i) {
+	    var current = $(this);
+	    $("#toc ul").append("<li><a id='link-" + i + "' href='#" + current.find('a').attr('name') + "' title='" + current.text() + "'>" + 
+	        current.text() + "</a></li>");
+	    current.find('a').addClass('up').attr('href', '#toc');
+	});
+});
